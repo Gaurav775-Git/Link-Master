a8e9061bb6bee18378234fde201e147cebc0b661
Gaurav775-Git <gaurav775.official@gmail.com>
2026-02-04 22:50:55 +0530

    initial setup done

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='min-h-screen bg-black text-white'>
+      <h1>hello everyone</h1>
+    </div>
+  )
+}
+
+export default App
