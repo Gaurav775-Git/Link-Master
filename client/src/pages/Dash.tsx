80169973971e84da53c3c16ffca3811de01b07f5
Gaurav775-Git <gaurav775.official@gmail.com>
2026-02-06 12:15:58 +0530

    loader fixed

--- client/src/pages/Dash.tsx
+++ client/src/pages/Dash.tsx
@@ -101,7 +101,7 @@ const Dash = () => {
         </div>
 
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
-          {loader ? filteredLinks.length === 0 ? (
+          {!loader ? filteredLinks.length === 0 ? (
             <div className='col-span-full text-center py-20'>
               <p className='text-xl text-gray-400'>No links found</p>
             </div>
